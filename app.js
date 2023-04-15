29b7bde504e5f82a5a38ecd3725a57afd56fa252
youngtruenox <co.lizama@duocuc.cl>
2023-04-15 19:15:04 -0400

    se modifica formulario

--- app.js
+++ app.js
@@ -12,27 +12,61 @@ var comentario = document.getElementById('comentario');
 var error = document.getElementById('error');
 error.style.color = 'red';
 
-//function enviarformulario(){
-//    console.log('enviando formulario...');
+function enviarFormulario(){
+    console.log ('Enviando formulario');
 
-//    var mensajeserror = [];
+    var mensajesError=[];
 
- //   if (nombre.value === null || nombre.value ===''){
- //       mensajeserror.push('engresa tu nombre');
- //   }
- //   if (password.value === null || password.value ===''){
- //       mensajeserror.push('engresa tu password');
- //   }
+    if(nombre.value===null || nombre.value==='' || nombre.value<2 && nombre.value>21){
+        mensajesError.push('Favor ingresa un nombre válido entre 3 y 20 caracteres');
+    }
 
- //   error.innerHTML = mensajeserror.join(', ' );
+    if(apellido_paterno.value===null || apellido_paterno.value==='' || apellidoP.value<2 && apellidoP.value>21){
+        mensajesError.push('Favor ingresa un apellido paterno válido entre 3 y 20 caracteres');
+    }
+
+    if(apellido_materno.value===null || apellido_materno.value==='' || apellidoM.value<2 && apellidoM.value>21){
+        mensajesError.push('Favor ingresa un apellido materno válido entre 3 y 20 caracteres');
+    }
+
+    if(rut.value===null || rut.value==='' || rut.value<8 && rut.value>11){
+        mensajesError.push('Favor ingresa un rut válido');
+    }
+    if(celular.value===null ||celular.value==='' || celular.value<8 && celular.value>13){
+        mensajesError.push('Favor ingresa un teléfono válido');
+    }
+    if(edad.value===null || edad.value==='' || edad.value<17 && edad.value>36){
+        mensajesError.push('Favor ingresa una edad entre 18 y 35 años');
+    }
+    if (fecha_nacimiento.value===null || fecha_nacimiento.value===''){
+        mensajesError.push('Favor ingresa tu fecha de nacimiento');
+    }
+    if (genero.value===null || genero.value===''){
+        mensajesError.push('Favor ingresa el género con el que te identificas');
+    }
+    if (email.value===null || email.value===''){
+        mensajesError.push('Favor ingresa tu correo');
+    }
+    if (profesion.value===null || profesion.value===''){
+        mensajesError.push('Favor ingresa tu profesión');
+    }
+    if (comentario.value===null || comentario.value===''){
+        mensajesError.push('Favor ingresa tu motivación para ser parte del equipo');
+    }
 
 
- //   return false;
-//}
+    error.innerHTML = mensajesError.join('<br>');
 
-var form = document.getElementById(formulario);
-    form.addEventListener("submit", function(evt){
-        evt.preventDefault();
-        console.log('enviando formulario...');
+    
 
-    });
+
+
+
+
+
+    return false;
+
+
+
+
+}
